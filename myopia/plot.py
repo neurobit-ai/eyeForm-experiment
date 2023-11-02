53c1fa949947c53f4cd72dadead5d35e80c0f632
Alisterchan <42401549+Alisterchan@users.noreply.github.com>
2023-11-02 03:21:05 +0000

    Replace index.html , report.html , ploy.py and table.py at 1st layer with oringin file

---
+++ myopia/plot.py
@@ -0,0 +1,353 @@
+from js import sex, age, y1, y2, records, suggestion, localStorage, report , language_type
+if 'ale' in sex:
+    sex = {'Male': '男', 'Female': '女'}[sex]
+#print(language_type)
+import pandas as pd
+import numpy as np
+import pickle
+from scipy.interpolate import make_interp_spline
+#with open('data_to_plot.pkl', 'rb') as f:
+    #db_version, slope_groupby, stacked_area = pickle.load(f)[report]
+with open('data_to_plot_20231013.pkl', 'rb') as f:
+    db_version , slope_groupby , results_groupby , stacked_area , constant_ci_groupby , coef_ci_025_groupby , coef_ci_975_groupby = pickle.load(f)[report]
+display(f'{db_version}', target='copyrightid')
+
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+
+#font_path = 'TaipeiSansTCBeta-Bold.ttf'
+if language_type== 0 :
+    font_path = 'msjhbd.ttc'
+    font_paht_s = "msjh.ttc"
+elif language_type== 2:
+    font_path = 'msyhbd.ttc'
+    font_paht_s = "msyh.ttc"
+else:
+    font_path = ''
+    font_paht_s = "" 
+#print("PATH IS " + font_path)
+custom_font = FontProperties(fname=font_path)
+custom_font_s = FontProperties(fname=font_paht_s)
+
+plt.figure(figsize=(8, 6))
+
+def moving_average_cal(data):
+  moving_average = []
+  window_sum = 0
+  window_count=0
+  length= len(data)
+  #print(data)
+  for index in range(0,length):
+    window_count = 0
+    window_sum = 0
+    if index - 2 >= 0:
+       window_sum += data.iloc[index - 2]
+       window_count+=1
+    if index - 1 >= 0:
+       window_sum += data.iloc[index - 1]
+       window_count+=1
+    window_sum += data.iloc[index]
+    window_count+=1
+    #print(data.iloc[index])
+    if index + 1 < length:
+       window_sum += data.iloc[index + 1]
+       window_count+=1
+    if index + 2 < length:
+       window_sum += data.iloc[index + 2]
+       window_count+=1
+    moving_average.append(window_sum/window_count)
+  #print(moving_average)
+  return moving_average
+
+def plot(sex, report):
+    area = stacked_area.loc[sex].loc[3:16]
+    MorF = {'男': 'Male', '女': 'Female'}[sex]
+    if report == '軸長':
+        area_index_smooth = np.linspace(area.index.min(), area.index.max(), 100)
+        splP100 = make_interp_spline(area.index, area['P100'], k=3)
+        splP90 = make_interp_spline(area.index, area['P90'], k=3)
+        splP75 = make_interp_spline(area.index, area['P75'], k=3)
+        splP50 = make_interp_spline(area.index, area['P50'], k=3)
+        area_100_smooth = splP100(area_index_smooth)
+        area_90_smooth = splP90(area_index_smooth)
+        area_75_smooth = splP75(area_index_smooth)
+        area_50_smooth = splP50(area_index_smooth)
+        plt.fill_between(area_index_smooth, area_100_smooth, area_90_smooth, color='red', alpha=0.2, label='High Risk')
+        plt.fill_between(area_index_smooth, area_90_smooth, area_75_smooth, color='yellow', alpha=0.2, label='Medium Risk')#orange
+        plt.fill_between(area_index_smooth, area_75_smooth, area_50_smooth, color='lightgreen', alpha=0.2, label='Low Risk')#yellow
+        #plt.fill_between(area_index_smooth, area_50_smooth, 19.5, color='lightgreen', alpha=0.2, label='0~50%')
+    else :
+        #area_index_smooth = np.linspace(area.index.min(), area.index.max(), 100)
+        #splP50 = make_interp_spline(area.index, area['P50'], k=3)
+        #splP25 = make_interp_spline(area.index, area['P25'], k=3)
+        #splP10 = make_interp_spline(area.index, area['P10'], k=3)
+        #area_50_smooth = splP50(area_index_smooth)
+        #area_25_smooth = splP25(area_index_smooth)
+        #area_10_smooth = splP10(area_index_smooth)
+        #plt.fill_between(area_index_smooth, area_50_smooth, 5, color='lightgreen', alpha=0.2, label='50~100%')
+        #plt.fill_between(area_index_smooth, area_25_smooth, area_50_smooth, color='yellow', alpha=0.2, label='25~50%')
+        #plt.fill_between(area_index_smooth, area_10_smooth, area_25_smooth, color='orange', alpha=0.2, label='10~25%')
+        #plt.fill_between(area_index_smooth, -9, area_10_smooth, color='red', alpha=0.2, label='0~10%')
+        area_100_smooth = moving_average_cal(area['P100'])
+        area_50_smooth = moving_average_cal(area['P50'])
+        area_25_smooth = moving_average_cal(area['P25'])
+        area_10_smooth = moving_average_cal(area['P10'])
+        area_0_smooth = moving_average_cal(area['P0'])
+        plt.fill_between(area.index, area_50_smooth, area_100_smooth, color='lightgreen', alpha=0.2, label='50~100%')
+        plt.fill_between(area.index, area_25_smooth, area_50_smooth, color='yellow', alpha=0.2, label='25~50%')
+        plt.fill_between(area.index, area_10_smooth, area_25_smooth, color='orange', alpha=0.2, label='10~25%')
+        plt.fill_between(area.index, area_0_smooth, area_10_smooth, color='red', alpha=0.2, label='0~10%')
+    #plt.title(f"Trend of {MorF} Children in Taiwan  {db_version}", fontsize=12)
+    if language_type== 0 :
+        if report == '軸長':
+            plt.title(f"{sex}童軸長成長趨勢", fontproperties=custom_font, fontsize=16)
+        if report == '球面度數':
+            plt.title(f"{sex}童球面度數成長趨勢", fontproperties=custom_font, fontsize=16)
+    elif language_type== 1:
+        if report == '軸長':
+            plt.title(f"Trend in axial length of {MorF} children", fontsize=16)
+        if report == '球面度數':
+            plt.title(f"Trend in spherical diopter of {MorF}children", fontsize=16)
+    elif language_type== 2:
+        if report == '軸長':
+            plt.title(f"{sex}童轴长成长趋势", fontproperties=custom_font, fontsize=16)
+        if report == '球面度數':
+            plt.title(f"{sex}童球面度数成长趋势", fontproperties=custom_font, fontsize=16)
+    else :
+        if report == '軸長':
+            plt.title(f"Trend in axial length of {MorF} children", fontsize=16)
+        if report == '球面度數':
+            plt.title(f"Trend in spherical diopter of {MorF} children", fontsize=16)
+
+risk = [...] * 4
+eye_word = [...] * 2
+#x_label=[ "ortho-k" ,"glasses" ,"multiple treatment" ,"no treatment", "atropine" ]
+sdl_coef=[ -0.54 ,-0.357 ,-0.392 , -0.696 , -0.424 ]
+sdl_coef_d=[ 0.944 ,0.968 ,0.694 , 1.007 , 0.73 ]
+sdr_coef=[ -0.685 ,-0.282 ,-0.617 , -0.419 , -0.436 ]
+sdr_coef_d=[ 1.178 , 0.658 , 0.617 , 0.84 , 0.817 ]
+all_coef=[ 0.092 , 0.14 , 0.13 , 0.141 , 0.253 ]
+all_coef_d=[ 0.169 , 0.331 , 0.17 , 0.203 , 1.178 ]
+alr_coef=[ 0.097 , 0.1 , 0.156 , 0.124 , 0.116 ]
+alr_coef_d=[ 0.213 , 0.185 , 0.236 , 0.18 , 0.173 ]
+
+if language_type== 0 :
+    if report == '軸長':
+        eye_word[0]= '右眼軸長'
+        eye_word[1]= '左眼軸長'
+    if report == '球面度數':
+        eye_word[0]= '右眼球面度數'
+        eye_word[1]= '左眼球面度數'
+    risk[0] = f'在年齡正常範圍內，屬低風險，建議一年定期檢查，無潛在近視發展狀況。'
+    risk[1] = f'稍長於年齡正常範圍，屬中風險，建議一年定期檢查，需改變生活型態及減少外在環境影響。'
+    risk[2] = f'長於年齡正常範圍，屬高風險，建議半年回診檢查，需改變生活型態及減少外在環境影響（例：電腦及手機使用時間需要注意並適度休息、戶外活動需要配戴太陽眼鏡防藍光、UV），並搭配葉黃素或魚油服用。'
+    risk[3] = f'甚長於年齡正常範圍，屬極高風險，有極高近視惡化發展可能，建議3個月回診檢查，需改變生活型態及減少外在環境影響（例：電腦及手機使用時間需要注意並適度休息、避免坐姿不正，戶外活動需要配戴太陽眼鏡防藍光、UV），搭配葉黃素或魚油服用，並搭配積極治療控制。'
+    samples_not_enought = '該年齡收案不足，無法提供具有統計意義之危險度分級。'
+elif language_type== 1:
+    if report == '軸長':
+        eye_word[0] = 'Axial length of right eye'
+        eye_word[1] = 'Axial length of left eye'
+    if report == '球面度數':
+        eye_word[0] = 'Spherical diopter of right eye'
+        eye_word[1] = 'Spherical diopter of left eye'
+    risk[0] = f' within the normal age range, it is considered low risk. Regular annual check-ups are recommended, and there is no indication of potential nearsightedness development.'
+    risk[1] = f' slightly beyond the normal age range, it is considered moderate risk. Regular annual check-ups are recommended, along with the need to adjust lifestyle and minimize external environmental influences.'
+    risk[2] = f' beyond the normal age range, it is considered high risk. Biannual follow-up check-ups are recommended, along with the need to adjust lifestyle and minimize external environmental influences (e.g., being mindful of screen time for computers and phones and taking breaks, wearing sunglasses to protect against blue light and UV rays during outdoor activities). Additionally, consider supplementing with lutein or fish oil.'
+    risk[3] = f' far beyond the normal age range, it is considered extremely high risk with a significant potential for severe myopia progression. Quarterly follow-up check-ups are recommended, along with the need to adjust lifestyle and minimize external environmental influences (e.g., being mindful of screen time for computers and phones and taking breaks, maintaining proper posture, wearing sunglasses to protect against blue light and UV rays during outdoor activities). Additionally, consider supplementing with lutein or fish oil, and adopting proactive treatment measures for control.'
+    samples_not_enought = 'The number of cases within this age group is insufficient to provide statistically significant risk classification.'
+elif language_type== 2:
+    if report == '軸長':
+        eye_word[0]= '右眼轴长'
+        eye_word[1]= '左眼轴长'
+    if report == '球面度數':
+        eye_word[0]= '左眼球面度数'
+        eye_word[1]= '左眼球面度数'
+    risk[0] = f'在年龄正常范围内，属低风险，建议一年定期检查，无潜在近视发展状况。'
+    risk[1] = f'稍长于年龄正常范围，属中风险，建议一年定期检查，需改变生活型态及减少外在环境影响。'
+    risk[2] = f'长于年龄正常范围，属高风险，建议半年回诊检查，需改变生活型态及减少外在环境影响（例：电脑及手机使用时间需要注意并适度休息、户外活动需要配戴太阳眼镜防蓝光、UV ），并搭配叶黄素或鱼油服用。'
+    risk[3] = f'甚长于年龄正常范围，属极高风险，有极高近视恶化发展可能，建议3个月回诊检查，需改变生活型态及减少外在环境影响（例：电脑及手机使用时间需要注意并适度休息、避免坐姿不正，户外活动需要配戴太阳眼镜防蓝光、UV），搭配叶黄素或鱼油服用，并搭配积极治疗控制。'
+    samples_not_enought = '该年龄收案不足，无法提供具有统计意义之危险度分级。'
+else :
+    if report == '軸長':
+        eye_word[0] = 'Axial length of right eye'
+        eye_word[1] = 'Axial length of left eye'
+    if report == '球面度數':
+        eye_word[0] = 'Spherical diopter of right eye'
+        eye_word[1] = 'Spherical diopter of left eye'
+    risk[0] = f' within the normal age range, it is considered low risk. Regular annual check-ups are recommended, and there is no indication of potential nearsightedness development.'
+    risk[1] = f' slightly beyond the normal age range, it is considered moderate risk. Regular annual check-ups are recommended, along with the need to adjust lifestyle and minimize external environmental influences.'
+    risk[2] = f' beyond the normal age range, it is considered high risk. Biannual follow-up check-ups are recommended, along with the need to adjust lifestyle and minimize external environmental influences (e.g., being mindful of screen time for computers and phones and taking breaks, wearing sunglasses to protect against blue light and UV rays during outdoor activities). Additionally, consider supplementing with lutein or fish oil.'
+    risk[3] = f' far beyond the normal age range, it is considered extremely high risk with a significant potential for severe myopia progression. Quarterly follow-up check-ups are recommended, along with the need to adjust lifestyle and minimize external environmental influences (e.g., being mindful of screen time for computers and phones and taking breaks, maintaining proper posture, wearing sunglasses to protect against blue light and UV rays during outdoor activities). Additionally, consider supplementing with lutein or fish oil, and adopting proactive treatment measures for control.'
+    samples_not_enought = 'The number of cases within this age group is insufficient to provide statistically significant risk classification.'
+Risk = {}
+import re
+def x(age):
+    m = re.match('(\d+)歲(\d+)', age)
+    return int(m.group(1)) + int(m.group(2)) / 12
+
+if round(x(age)) in range(3, 17):
+    if report == '軸長':
+        p0, p50, p75, p90, p100 = stacked_area.loc[sex].loc[round(x(age))]
+        for y, eye in (y1, eye_word[0]), (y2, eye_word[1]):
+            if y < p50:
+                display(f'{eye}{risk[0]}', target='advice')
+                localStorage.setItem(eye, 0)
+            elif y < p75:
+                display(f'{eye}{risk[1]}', target='advice')
+                localStorage.setItem(eye, 1)
+            elif y < p90:
+                display(f'{eye}{risk[2]}', target='advice')
+                localStorage.setItem(eye, 2)
+            else:
+                display(f'{eye}{risk[3]}', target='advice')
+                localStorage.setItem(eye, 3)
+    if report == '球面度數':
+        p100, p50, p25, p10, p0 = stacked_area.loc[sex].loc[round(x(age))]
+        for y, eye in (y1, eye_word[0]), (y2, eye_word[1]):
+            if y > p50:
+                display(f'{eye}{risk[0]}', target='advice')
+                localStorage.setItem(eye, 0)
+            elif y > p25:
+                display(f'{eye}{risk[1]}', target='advice')
+                localStorage.setItem(eye, 1)
+            elif y > p10:
+                display(f'{eye}{risk[2]}', target='advice')
+                localStorage.setItem(eye, 2)
+            else:
+                display(f'{eye}{risk[3]}', target='advice')
+                localStorage.setItem(eye, 3)
+else:
+    display(samples_not_enought, target='advice')
+    localStorage.setItem('右眼', '')
+    localStorage.setItem('左眼', '')
+
+#plt.figure(figsize=(7.5, 7.5))
+plot(sex, report)
+if suggestion == None :
+    suggestion='不處置'
+if suggestion == '不處置' :
+    label_index = 3
+elif suggestion == '低散瞳劑' or suggestion == '中散瞳劑' or suggestion == '高散瞳劑' :
+    label_index = 4
+elif suggestion == '一般眼鏡' :
+    label_index = 1
+elif suggestion == '一般眼鏡＋中散瞳劑' :
+    label_index = 2
+elif suggestion == '一般眼鏡散瞳劑' :
+    label_index = 2
+else :
+    label_index = 0
+agecounter = int(16-x(age))+2
+x_age_series=[0]*agecounter#this is for initial
+x_stdu_value_series=[0]*agecounter#this is for initial
+x_stdd_value_series=[0]*agecounter#this is for initial
+x_age_series[0]=x(age)
+odp_first=True
+osp_first=True
+
+for age_index in range(1,agecounter):
+    x_age_series[age_index] = x_age_series[age_index-1] + 1
+def C_I_cal(nowvalue):  
+    #print("In draw  constant_ci_groupby : "+str(constant_ci_groupby[sex][suggestion])+ "----coef_ci_025_groupby is "+ str(coef_ci_025_groupby[sex][suggestion])+ "----coef_ci_975_groupby is "+ str(coef_ci_975_groupby[sex][suggestion]))
+    constant_025=nowvalue-constant_ci_groupby[sex][suggestion]
+    constant_975=nowvalue+constant_ci_groupby[sex][suggestion]
+    max_value = constant_025 +  coef_ci_025_groupby[sex][suggestion]
+
+    min_value = constant_025 +  coef_ci_975_groupby[sex][suggestion]
+    if  constant_975 +  coef_ci_025_groupby[sex][suggestion]  > max_value :
+        max_value = constant_975 +  coef_ci_025_groupby[sex][suggestion]
+    if  constant_975 +  coef_ci_025_groupby[sex][suggestion]  < min_value :
+        min_value = constant_975 +  coef_ci_025_groupby[sex][suggestion]
+
+    if  constant_975 + coef_ci_975_groupby[sex][suggestion]  > max_value :
+        max_value = constant_975 + coef_ci_975_groupby[sex][suggestion]
+    if  constant_975 + coef_ci_975_groupby[sex][suggestion]  < min_value :
+        min_value = constant_975 + coef_ci_975_groupby[sex][suggestion]       
+
+    return max_value,min_value
+if y1 != "":
+    if y1 == 0 :
+        y1 = 19.5
+    plt.scatter(x(age), y1, color='red', label='OD now' , marker='D')
+    #print("od is : " + str(y1 ))
+if y2 != "":
+    if y2 == 0 :
+        y2 = 19.5
+    plt.scatter(x(age), y2, color='blue', label='OS now' , marker='D')
+    #print("os is : " + str(y2))
+if y1 != "" and slope_groupby[sex].get(suggestion):
+    x_stdu_value_series[0] = y1
+    x_stdd_value_series[0] = y1
+    for age_index in range(1,agecounter):
+        x_stdu_value_series[age_index],x_stdd_value_series[age_index] = C_I_cal(y1 + slope_groupby[sex][suggestion] * (age_index-1))
+        if odp_first :
+            plt.scatter(x_age_series[age_index], y1 + slope_groupby[sex][suggestion] * age_index, color='red', alpha=0.5, label='OD in 1 yr', marker='*')
+            odp_first=False
+        else :
+            plt.scatter(x_age_series[age_index], y1 + slope_groupby[sex][suggestion] * age_index, color='red', alpha=0.5, marker='*')
+    plt.fill_between(x_age_series, x_stdu_value_series, x_stdd_value_series, color='#E84F6B', alpha=0.4, label='OD in future')##AC92EB
+    #plt.scatter(x(age) + 1, y1 + slope_groupby[sex][suggestion], color='red', label='OD in 1 yr', marker='*')
+if y2 != "" and slope_groupby[sex].get(suggestion):
+    x_stdu_value_series[0] = y2
+    x_stdd_value_series[0] = y2
+    for age_index in range(1,agecounter):
+        x_stdu_value_series[age_index],x_stdd_value_series[age_index] = C_I_cal(y2 + slope_groupby[sex][suggestion] * (age_index-1))
+        if osp_first :
+            plt.scatter(x_age_series[age_index], y2 + slope_groupby[sex][suggestion] * age_index, color='blue', alpha=0.5, label='OS in 1 yr', marker='*')
+            osp_first=False
+        else :
+            plt.scatter(x_age_series[age_index], y2 + slope_groupby[sex][suggestion] * age_index, color='blue', alpha=0.5, marker='*')
+    plt.fill_between(x_age_series, x_stdu_value_series, x_stdd_value_series, color='#4FC1E8', alpha=0.4, label='OS in future')
+    #plt.scatter(x(age) + 1, y2 + slope_groupby[sex][suggestion], color='blue', label='OS in 1 yr', marker='*')
+import json
+records = json.loads(records)
+od, os = (18, 24) if report == '軸長' else (15, 21)
+odp_first=True
+osp_first=True
+for record in records:
+    if record[od] != "":
+        if odp_first :
+            plt.scatter(x(record[11]), record[od], color='red', marker='.' , label='OD in past')
+            odp_first=False
+        else :
+            plt.scatter(x(record[11]), record[od], color='red', marker='.')
+        #print("od is : " + str(record[od]) )
+    if record[os] != "":
+        if osp_first :
+            plt.scatter(x(record[11]), record[os], color='blue', marker='.', label='OS in past')
+            osp_first=False
+        else :
+            plt.scatter(x(record[11]), record[os], color='blue', marker='.')
+        #print("os is : " + str(record[os]))
+plt.plot(3, 19.5 , linestyle='none' , marker='None', alpha=0, label=db_version)
+if report == '軸長':
+    plt.legend(loc='center right' , bbox_to_anchor=(1.19, 0.25),fontsize=8)
+if report == '球面度數':
+    plt.legend(loc='center right' , bbox_to_anchor=(1.19, 0.25),fontsize=8)
+plt.xticks(range(3, 17 if x(age) + 1 <= 16 else int(x(age)) + 2))
+plt.yticks(range(20, 30) if report == '軸長' else range(-8, 7))
+if language_type== 0 :
+    plt.xlabel("年齡 (歲)", fontproperties=custom_font_s, fontsize=12)
+elif language_type== 2:
+    plt.xlabel("年龄 (岁)", fontproperties=custom_font_s, fontsize=12)
+else :
+    plt.xlabel("Age (years)", fontsize=12)
+#plt.xlabel('Age', fontsize=12)
+if language_type== 0 :
+    plt.ylabel('軸長 (mm)' if report == '軸長' else '球面度數 (度)', fontproperties=custom_font_s, fontsize=12)
+elif language_type== 2:
+    plt.ylabel('轴长 (mm)' if report == '軸長' else '球面度数 (度)', fontproperties=custom_font_s, fontsize=12)
+else :
+    plt.ylabel('Axial Length (mm)' if report == '軸長' else 'SPH (degrees)', fontsize=12)
+#plt.ylabel('Axial Length' if report == '軸長' else 'SPH', fontsize=12)
+plt.margins(0)
+plt.subplots_adjust(left=0.1, right=0.86, bottom=0.1, top=0.9, wspace=0.8, hspace=0.2)
+if report == '軸長':
+    plt.ylim(19.5, 30.5)
+    plt.xlim(3, 16)
+    plt.text(16 if x(age) + 1 < 16 else x(age) + 1, 18.8 if sex=='女' else 19.2, f'', horizontalalignment='right', fontsize=8)
+if report == '球面度數':
+    plt.ylim(-9, 5)
+    plt.xlim(3, 16)
+    plt.text(16 if x(age) + 1 < 16 else x(age) + 1, -10.5 if sex=='女' else -10.5, f'', horizontalalignment='right', fontsize=8)
+display(plt, target='plot')
--- table.py
+++ table.py
@@ -1,179 +1,27 @@
-from js import sex, age, y1, y2, records, report , language_type, suggestion
-if 'ale' in sex:
-    sex = {'Male': '男', 'Female': '女'}[sex]
+from js import sex, age, y1, y2, records, report
+
 import re
-import pickle
-with open('data_to_plot.pkl', 'rb') as f:
-    db_version, slope_groupby, stacked_area = pickle.load(f)[report]
-
-def x(age):
+def _y_m(age):
     m = re.match('(\d+)歲(\d+)', age)
-    return int(m.group(1)) + int(m.group(2)) / 12
+    return f'{m.group(1)}y{m.group(2)}m__'
 
-def _y_m(age,year,sign):
-    m = re.match('(\d+)歲(\d+)', age)
-    if int(m.group(1))+year > 9:
-        space01=''
-    else :
-        space01='&nbsp'
-    if int(m.group(2)) > 9:
-        space02=''
-    else :
-        space02='&nbsp'
-    m_y=str(int(m.group(1))+year)
-    if language_type== 0 :
-        return f'&nbsp{space01}{m_y}歲{space02}{m.group(2)}個月{sign}'
-    elif language_type== 2:
-        return f'&nbsp{space01}{m_y}岁{space02}{m.group(2)}个月{sign}'
-    else:
-        return f'&nbsp{space01}{m_y}y{space02}{m.group(2)}m{sign}'
-    
-#print(language_type)
 table = {}
-sign='⯁'
-table['Age　'] = [f'{_y_m(age,0,sign)}']
-if y1 == 19.5 :
-    table['OD'] = [f'<20']
-elif y1== 30.5 :
-    table['OD'] = [f'>30']
-else :
-    table['OD'] = [f'{y1:.2f}']
-
-if y2 == 19.5 :
-    table['OS'] = [f'<20']
-elif y2 == 30.5 :
-    table['OS'] = [f'>30']
-else :
-    table['OS'] = [f'{y2:.2f}']
-
-
-agecounter = int(16-x(age))+1
-OD_new=y1
-OS_new=y2
-sign='★'
-for age_index in range(1,agecounter):
-    table['Age　'].append(f'{_y_m(age,age_index,sign)}')
-    OD_new += slope_groupby[sex][suggestion]
-    OS_new += slope_groupby[sex][suggestion]
-    if report == '軸長':
-        if OD_new <= 19.5 :
-            table['OD'].append(f'<20')
-        elif OD_new >= 30.5 :
-            table['OD'].append(f'>30')
-        else :
-            table['OD'].append(f'{OD_new:.2f}')
-
-        if OS_new <= 19.5 :
-            table['OS'].append(f'<20')
-        elif OS_new >= 30.5 :
-            table['OS'].append(f'>30')
-        else :
-            table['OS'].append(f'{OS_new:.2f}')
-    else:
-        if OD_new <= -9:
-            table['OD'].append(f'<-9')
-        elif OD_new >= 5 :
-            table['OD'].append(f'>5')
-        else :
-            table['OD'].append(f'{OD_new:.2f}')
-
-        if OS_new <= -9 :
-            table['OS'].append(f'<-9')
-        elif OS_new >= 5 :
-            table['OS'].append(f'>5')
-        else :
-            table['OS'].append(f'{OS_new:.2f}')
-#print("*********************")
-#print(type(records))
-
-#print(type(records))
+table['Age__'] = [f'{_y_m(age)}']
+table['OD'] = [f'{y1:.2f}']
+table['OS'] = [f'{y2:.2f}']
 
 import json
 records = json.loads(records)
-sign='●'
 od, os = (18, 24) if report == '軸長' else (15, 21)
 for record in records:
     if record[od] or record[os]:
-        table['Age　'].insert(0, f'{_y_m(record[11],0,sign)}')
-        if report == '軸長':
-            if record[od] == 19.5 :
-                table['OD'].insert(0, f'<20')
-            elif record[od] == 30.5 :
-                table['OD'].insert(0, f'>30')
-            else :
-                table['OD'].insert(0, f'{record[od]:.2f}')
+        table['Age__'].insert(0, f'{_y_m(record[11])}')
+        table['OD'].insert(0, f'{record[od]:.2f}')
+        table['OS'].insert(0, f'{record[os]:.2f}')
 
-            if record[os] == 19.5 :
-                table['OS'].insert(0, f'<20')
-            elif record[os] == 30.5 :
-                table['OS'].insert(0, f'>30')
-            else :
-                table['OS'].insert(0, f'{record[os]:.2f}')
-        else:
-            if record[od] == -9 :
-                table['OD'].insert(0, f'<-9')
-            elif record[od] == 5 :
-                table['OD'].insert(0, f'>5')
-            else :
-                table['OD'].insert(0, f'{record[od]:.2f}')
-
-            if record[os] == -9 :
-                table['OS'].insert(0, f'<-9')
-            elif record[os] == 5 :
-                table['OS'].insert(0, f'>5')
-            else :
-                table['OS'].insert(0, f'{record[os]:.2f}')
 import pandas as pd
 df = pd.DataFrame(table)
-if report == '軸長':
-    new_column_names = {'OD': 'OD(mm)　', 'OS': 'OS(mm)　'}
-elif language_type == 0 or language_type == 2:
-    new_column_names = {'OD': 'OD(度)　', 'OS': 'OS(度)　'}
-else :
-    new_column_names = {'OD': 'OD(degrees)　', 'OS': 'OS(degrees)　'}
-df.rename(columns=new_column_names, inplace=True)
-
 df = df.T
-#print(df.index)
-
-if language_type == 0 :
-    new_index_names  = {'Age　': '年齡　'}
-    df.rename(index=new_index_names, inplace=True)
-    df.columns = df.loc['年齡　']
-    #df = df.drop(index='年齡　')
-elif  language_type == 2:
-    new_index_names  = {'Age　': '年龄　'}
-    df.rename(index=new_index_names, inplace=True)
-    df.columns = df.loc['年龄　']
-    #df = df.drop(index='年龄　')
-else:
-    df.columns = df.loc['Age　']
-    #df = df.drop(index='Age　')
-
-left_top_element = df.iloc[0, 0]
-#print(left_top_element)
-#print(df.index)
-duplicated_columns = df.columns[df.columns.duplicated()]
-if not duplicated_columns.empty:
-    renamed_columns  = df.columns
-    for col_name  in duplicated_columns:
-        count = 1
-        while col_name  in renamed_columns:
-            renamed_columns = renamed_columns.to_list()
-            renamed_columns[renamed_columns.index(col_name)] = f"{col_name}_v{count}"
-            renamed_columns = pd.Index(renamed_columns)
-            count += 1
-    df.columns = renamed_columns
-
-#print(df.index)
-styled_df = df.style.hide(axis="columns")
-styled_df = styled_df.set_properties(**{
-'background-color': 'white', 
-'color': 'black',
-'text-align': 'center',
-'border': 'solid',
-'border-width': '2px',
-'padding': '2px'
-})
-
-display(styled_df, target='table')
+df.columns = df.loc['Age__']
+df = df.drop(index='Age__')
+display(df, target='table')
