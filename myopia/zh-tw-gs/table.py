2f27fc7a15871ed829d30532bcba320279de6ce8
Alisterchan <42401549+Alisterchan@users.noreply.github.com>
2024-09-18 03:20:10 +0000

    Upgrade report.html and table.01

--- myopia/zh-tw-gs/table.py
+++ myopia/zh-tw-gs/table.py
@@ -23,7 +23,7 @@ def _y_m(age,year,sign):
         space02='&nbsp'
     m_y=str(int(m.group(1))+year)
     if language_type== 0 :
-        return f'&nbsp{space01}{m_y}歲{space02}{m.group(2)}個月{sign}'
+        return f'&nbsp{space01}{m_y}歲{space02}　<br>{m.group(2)}個月{sign}'
     elif language_type== 2:
         return f'&nbsp{space01}{m_y}岁{space02}{m.group(2)}个月{sign}'
     else:
@@ -47,6 +47,20 @@ elif y2 == 30.5 :
 else :
     table['OS'] = [f'{y2:.2f}']
 
+if y1 == 19.5 :
+    table['OD_m'] = [f'<20']
+elif y1== 30.5 :
+    table['OD_m'] = [f'>30']
+else :
+    table['OD_m'] = [f'{y1:.2f}']
+
+if y2 == 19.5 :
+    table['OS_m'] = [f'<20']
+elif y2 == 30.5 :
+    table['OS_m'] = [f'>30']
+else :
+    table['OS_m'] = [f'{y2:.2f}']
+
 def logarithm_calculate_middle(age_index , age_origin): 
     difference = np.exp(data_al[report][sex][suggestion]["bo"]+np.log(age_index)*data_al[report][sex][suggestion]["b1"]) - np.exp(data_al[report][sex][suggestion]["bo"]+np.log(age_origin)*data_al[report][sex][suggestion]["b1"])
     return difference
@@ -57,20 +71,28 @@ if agecounter <= 0:
 OD_new=y1
 OS_new=y2
 sign=' '
+
+OD_no_management_data=[0]*agecounter#this is for initial
+OS_no_management_data=[0]*agecounter#this is for initial
+OD_no_management_data[0] = y1
+OS_no_management_data[0] = y2
+
 OD_control_data=[0]*agecounter#this is for initial
 OS_control_data=[0]*agecounter#this is for initial
 OD_control_data[0] = y1
 OS_control_data[0] = y2
 if  round(x(age)) in range(3,17):
-    # OD_control_data = curve_calculation(OD_control_data,current_slope_rate_OD,current_slope_attu_OD,int(x(age)),control_rate)
-    # OS_control_data = curve_calculation(OS_control_data,current_slope_rate_OS,current_slope_attu_OS,int(x(age)),control_rate)
-    OD_control_data = curve_calculation(OD_control_data,current_slope_rate_OD,current_slope_attu_OD,int(x(age)),0)
-    OS_control_data = curve_calculation(OS_control_data,current_slope_rate_OS,current_slope_attu_OS,int(x(age)),0)
+    OD_no_management_data = curve_calculation(OD_no_management_data,current_slope_rate_OD,current_slope_attu_OD,int(x(age)),0)
+    OS_no_management_data = curve_calculation(OS_no_management_data,current_slope_rate_OS,current_slope_attu_OS,int(x(age)),0)
+    OD_control_data = curve_calculation(OD_control_data,current_slope_rate_OD,current_slope_attu_OD,int(x(age)),control_rate)
+    OS_control_data = curve_calculation(OS_control_data,current_slope_rate_OS,current_slope_attu_OS,int(x(age)),control_rate)
     
     for age_index in range(1,agecounter):
         table['Age　'].append(f'{_y_m(age,age_index,sign)}')
-        OD_new=OD_control_data[age_index]
-        OS_new=OS_control_data[age_index]
+        OD_new=OD_no_management_data[age_index]
+        OS_new=OS_no_management_data[age_index]
+        OD_new_control=OD_control_data[age_index]
+        OS_new_control=OS_control_data[age_index]
         if report == '軸長':
             if OD_new <= 19.5 :
                 table['OD'].append(f'<20')
@@ -99,6 +121,20 @@ if  round(x(age)) in range(3,17):
                 table['OS'].append(f'>5')
             else :
                 table['OS'].append(f'{OS_new:.2f}')
+                
+            if OD_new_control <= -9:
+                table['OD_m'].append(f'<-9')
+            elif OD_new >= 5 :
+                table['OD_m'].append(f'>5')
+            else :
+                table['OD_m'].append(f'{OD_new_control:.2f}')
+
+            if OS_new_control <= -9 :
+                table['OS_m'].append(f'<-9')
+            elif OS_new >= 5 :
+                table['OS_m'].append(f'>5')
+            else :
+                table['OS_m'].append(f'{OS_new_control:.2f}') 
 #print("*********************")
 #print(type(records))
 
@@ -132,19 +168,20 @@ for record in records:
                 table['OD'].insert(0, f'>5')
             else :
                 table['OD'].insert(0, f'{record[od]:.2f}')
-
+            table['OD_m'].insert(0,'　')
             if record[os] == -9 :
                 table['OS'].insert(0, f'<-9')
             elif record[os] == 5 :
                 table['OS'].insert(0, f'>5')
             else :
                 table['OS'].insert(0, f'{record[os]:.2f}')
+            table['OS_m'].insert(0,'　　')
 import pandas as pd
 df = pd.DataFrame(table)
 if report == '軸長':
     new_column_names = {'OD': 'OD(mm)　', 'OS': 'OS(mm)　'}
 elif language_type == 0 or language_type == 2:
-    new_column_names = {'OD': '右眼(D)　', 'OS': '左眼(D)　'}
+    new_column_names = {'OD': '右眼(D)　<br>無控制', 'OS': '左眼(D)　<br>無控制','OD_m':'右眼(D)　<br>有控制','OS_m':'左眼(D)　<br>有控制'}
 else :
     new_column_names = {'OD': 'OD(degrees)　', 'OS': 'OS(degrees)　'}
 df.rename(columns=new_column_names, inplace=True)
@@ -182,6 +219,13 @@ if not duplicated_columns.empty:
     df.columns = renamed_columns
 
 #print(df.index)
+def hide_border(val):
+    if val == '　':  # 當值為2時隱藏左方和下方框線
+        return 'border-left: none; border-right: none; border-bottom: none;'
+    elif val == '　　':  # 當值為2時隱藏左方和下方框線
+        return 'border-left: none; border-right: none; border-bottom: none; border-top: none;'
+    else:
+        return ''
 styled_df = df.style.hide(axis="columns")
 styled_df = styled_df.set_properties(**{
 'background-color': 'white', 
@@ -191,5 +235,5 @@ styled_df = styled_df.set_properties(**{
 'border-width': '2px',
 'padding': '2px'
 })
-
+styled_df = styled_df.applymap(hide_border)
 display(styled_df, target='table')
